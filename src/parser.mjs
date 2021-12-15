1107d8a0d6ec43814467169784b31f1e6e611f0c
Jim Krayer <jameskrayer@yahoo.com>
2021-12-15 18:58:39 -0500

    Add notes lookups. But all may change.

--- src/parser.mjs
+++ src/parser.mjs
@@ -11,10 +11,59 @@ import {
   prop,
   reduce,
   replace,
+  split,
   test,
   toPairs,
 } from "ramda";
 import eleventy from "./generators.mjs";
+
+// ----- Helpers -----
+
+// isLog:: String -> Boolean
+const isLog = test(/^id/);
+
+// notEmpty:: Any -> Boolean
+const notEmpty = compose(not, isEmpty);
+
+// notNIl:: Any -> Boolean
+const notNil = compose(not, isNil);
+
+// hasContent:: Logentry -> Boolean
+const hasContent = (x) => notEmpty(x.p) || notNil(x.linklist); // propSatisfies(notNil, "linklist");
+
+// hasLogentry:: CalendarItem -> Boolean
+const hasLogentry = compose(hasContent, pathOr({}, ["logentry"]));
+
+// getText:: Object -> String
+const getText = prop("_text");
+
+// getLink:: Object -> String
+const getLink = prop("link");
+
+// joinNewLine:: Array -> String
+const joinNewLine = join("\n\n");
+
+// getId:: Link -> String
+const getId = path(["_attributes", "recordname"]);
+
+// ----- Parsers -----
+
+// getParsedEntry:: Id -> FormattedText
+let getParsedEntry;
+
+// initFinder:: CampaignJSON -> Id -> Resource
+const initFinder = (campaignFile) => (id) =>
+  path(["root", ...id.split(".")], campaignFile);
+
+// parseP:: Paragraph -> String
+const parseP = (p) =>
+  Array.isArray(p) ? joinNewLine(map(getText, p)) : getText(p);
+
+// parseLL:: linkedlist -> String
+const parseLL = (ll) =>
+  Array.isArray(ll.link)
+    ? join("\n\n", map(getText, ll.link))
+    : getText(ll.link);
 
 // const title = replace(/'|\s|:/g, "");
 // const noteName = compose(title, path(["name", 0, "_"]));
@@ -30,36 +79,50 @@ import eleventy from "./generators.mjs";
 //   path(["root", "notes", 0])
 // );
 
-const isLog = test(/^id/);
-const notEmpty = compose(not, isEmpty);
-const notNil = compose(not, isNil);
-const hasContent = (x) => notEmpty(x.p) || notNil(x.linklist); // propSatisfies(notNil, "linklist");
-const hasLogentry = compose(hasContent, pathOr({}, ["logentry"]));
-const getText = prop("_text");
+const parseEntry = (entry) => {
+  const entryTitle = `## ${pathOr("", ["name", "_text"], entry)}`;
+  const { p, linklist } = entry.text;
 
-const parseP = (p) =>
-  Array.isArray(p) ? join("\n\n", map(getText, p)) : getText(p);
+  // console.log("entry", entry);
+  // console.log("entry?title", entryTitle);
+  // console.log("entry?p", p);
+  // console.log("entry?ll", linklist);
+};
 
-const parseLL = (ll) =>
-  Array.isArray(ll.link)
-    ? join("\n\n", map(getText, ll.link))
-    : getText(ll.link);
-
+// parseLogEntry:: CalendarEntry -> String
 const parseLogEntry = (logentry) => {
+  const { p, linklist } = logentry;
   let body = "";
 
-  if (!isNil(logentry.p)) {
+  if (!isNil(p)) {
     body = `${body}${parseP(logentry.p)}`;
   }
 
-  if (!isNil(logentry.linklist)) {
-    body = `${body}${parseLL(logentry.linklist)}`;
+  if (!isNil(linklist)) {
+    const links = Array.isArray(linklist)
+      ? linklist.flatMap(getLink)
+      : [].concat(getLink(linklist));
+
+    links.map(getParsedEntry);
+
+    // body = `${body}\n\n${getEntries(links)}`
+
+    // body = `${body}\n\n${entries.join()}`;
+
+    // links.map(compose(joinNewLine, parseEntry, getEntry, getId));
+
+    // joinNewLine(entries);
+
+    // console.log("links", links);
+
+    // console.log("LL", JSON.stringify(logentry.linklist));
+    // body = `${body}${parseLL(logentry.linklist)}`;
   }
 
   return body;
 };
 
-//
+// getCalendar:: FantasyGroundsDB -> [Id, String][]
 const getCalendar = compose(
   map(([key, { logentry, name }]) => [
     key,
@@ -70,9 +133,13 @@ const getCalendar = compose(
   pathOr({}, ["root", "calendar", "log"])
 );
 
-//
 const parseCalendar = (campaignFile) => {
+  getParsedEntry = compose(parseEntry, initFinder(campaignFile), getId);
+
   return getCalendar(campaignFile);
 };
 
 export default parseCalendar;
+
+// (fn, [a, b]) => return [fn(a), b]
+// merge link lists
